4fa679c4066cf1a535f988df13747ada63d3dbe6
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-13 13:40:16 -0700

    lecture 47 3:56 SignedIn Menu = () => error

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink, Link } from 'react-router-dom';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+import SignedInMenu from '../Menus/SignedInMenu';
 
 class NavBar extends Component {
+  state = {
+    authenticated: false
+  }
   render() {
+    const { authenticated } = this.state;
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={Link} to='/' header>
             <img src="assets/logo.png" alt="logo" />
             Re-vents
                </Menu.Item>
-          <Menu.Item name="Events" />
+          <Menu.Item as={NavLink} to='/events' name="Events" />
+          <Menu.Item as={NavLink} to='/people' name="People" />
           <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
+            <Button as={Link} to='/createEvent' floated="right" positive inverted content="Create Event" />
           </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
-          </Menu.Item>
+          {authenticated ? <SignedInMenu /> : <SignedOutMenu />}
         </Container>
       </Menu>
     );
