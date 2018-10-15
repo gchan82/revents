e786708445d48ff0e5c41403433a6ad3cefe7fbf
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-15 13:39:48 -0700

    End of Section 6

--- src/features/nav/Menus/SignedOutMenu.jsx
+++ src/features/nav/Menus/SignedOutMenu.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Menu, Button } from 'semantic-ui-react';
-function SignedOutMenu = () => {
+const SignedOutMenu = ({signIn}) => {
   return (
     <Menu.Item position="right">
-      <Button basic inverted content="Login" />
+      <Button onClick={signIn} basic inverted content="Login" />
       <Button basic inverted content="Register" style={{ marginLeft: '0.5em' }} />
     </Menu.Item>
   );
