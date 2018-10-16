3a82621587c3b48535f123ef665c687da4b6ab24
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-16 16:40:15 -0700

    Lecture 66 7:15 blank /event/1 page

--- src/features/EventList/EventListItem.jsx
+++ src/features/EventList/EventListItem.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import EventListAttendee from './EventListAttendee';
 
 export class EventListItem extends Component {
   render() {
-    const { event, onEventOpen, deleteEvent } = this.props;
+    const { event, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -34,7 +35,7 @@ export class EventListItem extends Component {
         <Segment clearing>
           <span>{event.description}</span>
           <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
-          <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
+          <Button as={Link} to={`/event/${event.id}`} color="teal" floated="right" content="View" />
         </Segment>
       </Segment.Group>
     );
