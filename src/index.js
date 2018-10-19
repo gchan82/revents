875b6f8e0fbc253c4fcb18f93f9af88899a8a7da
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-10-18 17:51:31 -0700

    End of Lecture 73

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
-import configureStore from './app/store/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import configureStore from "./app/store/configureStore";
+import ScrollToTop from "./app/common/util/scrollToTop";
 
 const store = configureStore();
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
-    </Provider>
-    , rootEl);
-}
+    </Provider>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', () => {
-    setTimeout(render)
-  })
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
