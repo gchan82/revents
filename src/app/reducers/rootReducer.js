c04372118f9c152b235fa77400df2a6d68f95f12
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-01 16:45:00 -0700

    End of Lectur 100

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,6 +3,7 @@ import {reducer as FormReducer} from 'redux-form';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 import modalsReducer from '../../features/modals/modalReducer';
+import authReducer from '../../features/auth/authReducer';
 
 const rootReducer = combineReducers({
   form: FormReducer,
